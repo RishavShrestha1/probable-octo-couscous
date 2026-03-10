d79269c0f95bb3ee0100e301989aee01522465ba
allForOne-alt <weeesdom1@gmail.com>
2026-03-10 21:57:26 +0545

    WHAT THE FUCK!!!!!!!!!!

--- main.cpp
+++ main.cpp
@@ -84,6 +84,9 @@ int main(int argc, char* argv[]){
                 // SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
                 // SDL_RenderClear(renderer);
 
+                player.render(renderer);
+                enemy.render(renderer);
+
                 // SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
                 // SDL_RenderDrawRect(renderer, &rect);
 
